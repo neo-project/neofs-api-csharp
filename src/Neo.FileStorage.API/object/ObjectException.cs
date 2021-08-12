5abde41d06ecb00b1438ca665b7455f171f80e08
ZhangTao1596 <zhangtao@ngd.neo.org>
2021-08-12 19:20:30 +0800

    fix object exception

--- src/Neo.FileStorage.API/object/ObjectException.cs
+++ src/Neo.FileStorage.API/object/ObjectException.cs
@@ -1,8 +1,22 @@
+#nullable enable
 using System;
 
 namespace Neo.FileStorage.API.Object
 {
-    public class SplitInfoException : Exception
+    public class ObjectExcpetion : Exception
+    {
+        public const string AlreadyRemovedError = "object already removed";
+        public const string RangeOutOfBoundsError = "range out of bounds";
+        public const string NotFoundError = "object not found";
+        public const string SizeExceedLimitError = "object size exceed limit";
+        public const string UnknownTypeError = "unknown object type";
+
+        public ObjectExcpetion() : base() { }
+
+        public ObjectExcpetion(string? message) : base(message) { }
+    }
+
+    public class SplitInfoException : ObjectExcpetion
     {
         public SplitInfo SplitInfo { get; private set; }
 
@@ -12,37 +26,37 @@ namespace Neo.FileStorage.API.Object
         }
     }
 
-    public class ObjectAlreadyRemovedException : Exception
+    public class ObjectAlreadyRemovedException : ObjectExcpetion
     {
-        public override string Message => "object already removed";
+        public override string Message => AlreadyRemovedError;
 
         public ObjectAlreadyRemovedException() : base() { }
     }
 
-    public class RangeOutOfBoundsException : Exception
+    public class RangeOutOfBoundsException : ObjectExcpetion
     {
-        public override string Message => "range out of bounds";
+        public override string Message => RangeOutOfBoundsError;
 
         public RangeOutOfBoundsException() : base() { }
     }
 
-    public class ObjectNotFoundException : Exception
+    public class ObjectNotFoundException : ObjectExcpetion
     {
-        public override string Message => "object not found";
+        public override string Message => NotFoundError;
 
         public ObjectNotFoundException() : base() { }
     }
 
-    public class ObjectSizeExceedLimitException : Exception
+    public class SizeExceedLimitException : ObjectExcpetion
     {
-        public override string Message => "object already removed";
+        public override string Message => SizeExceedLimitError;
 
-        public ObjectSizeExceedLimitException() : base() { }
+        public SizeExceedLimitException() : base() { }
     }
 
-    public class UnknownObjectTypeException : Exception
+    public class UnknownObjectTypeException : ObjectExcpetion
     {
-        public override string Message => "unknown object type";
+        public override string Message => UnknownTypeError;
 
         public UnknownObjectTypeException() : base() { }
     }
