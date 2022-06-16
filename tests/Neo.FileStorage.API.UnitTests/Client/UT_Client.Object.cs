ee5d442bcf1c80224f1f77cc03cac003b58318e4
ZhangTao1596 <zhangtao@ngd.neo.org>
2022-06-16 14:31:05 +0800

    update test

--- tests/Neo.FileStorage.API.UnitTests/Client/UT_Client.Object.cs
+++ tests/Neo.FileStorage.API.UnitTests/Client/UT_Client.Object.cs
@@ -46,8 +46,8 @@ namespace Neo.FileStorage.API.UnitTests.FSClient
             using var client = new Client.Client(key, host);
             using var source = new CancellationTokenSource();
             source.CancelAfter(TimeSpan.FromMinutes(10));
-            var session = client.CreateSession(20).Result;
-            var o = client.PutObject(obj, new CallOptions { Ttl = 1, Session = session }, source.Token).Result;
+            var session = client.CreateSession(uint.MaxValue).Result;
+            var o = client.PutObject(obj, new CallOptions { Ttl = 2, Session = session }, source.Token).Result;
             Console.WriteLine(o.String());
             Assert.AreNotEqual("", o.String());
         }
@@ -197,7 +197,7 @@ namespace Neo.FileStorage.API.UnitTests.FSClient
             using var client = new Client.Client(key, host);
             using var source = new CancellationTokenSource();
             source.CancelAfter(TimeSpan.FromMinutes(1));
-            var o = client.GetObjectHeader(address, false, false, new CallOptions { Ttl = 1 }, source.Token).Result;
+            var o = client.GetObjectHeader(address, false, false, new CallOptions { Ttl = 2 }, source.Token).Result;
             Console.WriteLine(o);
             Assert.AreEqual(oid, o.ObjectId);
         }
--- tests/Neo.FileStorage.API.UnitTests/Client/UT_Client.cs
+++ tests/Neo.FileStorage.API.UnitTests/Client/UT_Client.cs
@@ -12,23 +12,26 @@ namespace Neo.FileStorage.API.UnitTests.FSClient
     [TestClass]
     public partial class UT_Client
     {
-        // private readonly string host = "https://st1.storage.fs.neo.org:8082";
-        private readonly string host = "https://rpc01.morph.testnet.fs.neo.org:51331";
+        // private readonly string host = "https://st1.storage.fs.neo.org:8082"; //mainnet
+        // private readonly string host = "http://st04.testnet.fs.neo.org:8080"; //testnet
         // private readonly string host = "http://s01.neofs.devenv:8080";
-        // private readonly string host = "http://localhost:8081";
+        private readonly string host = "http://localhost:8081";
+        // private readonly ContainerID cid = ContainerID.FromString("CDiEce9WSF9FySHKUJtuzHbVLhFTzSXwiyj91HaZAg4A");//testnet t4
+        // private readonly ObjectID oid = ObjectID.FromString("3Y7k2oTXzAHSzaiMHnFJDBuaX6SxuByVPdRUsYcvYUbs");//testnet t4
+
         // private readonly ContainerID cid = ContainerID.FromString("8tqfNtRyBFhzRZnFkddVBxsYEQWjJes37Lfdq3p9R7Ge");//not in policy
         // private readonly ObjectID oid = ObjectID.FromString("CkQsya9r9cAgiHqk5GLXY34zdTWUKwQ7KmVSsMXfrKVe");//small
         // private readonly ObjectID oid = ObjectID.FromString("");//big
         // private readonly ObjectID oid = ObjectID.FromString("HVXgYePb1FKSidNWwdHD6tHNnFN6kfN2J6GUvc8Tg8ym");//split
-        private readonly ContainerID cid = ContainerID.FromString("4HTzRAK4uayjSsPKDrRPsrR44ey4DuNbGT24Vne6vs3Z");//the only one in policy
-        private readonly ObjectID oid = ObjectID.FromString("GUDTN2pXGFyQRx6vBUaDuKqCNWFV8J9e6Y2R6oFE2Mhc");//small
+        private readonly ContainerID cid = ContainerID.FromString("CDiEce9WSF9FySHKUJtuzHbVLhFTzSXwiyj91HaZAg4A");//the only one in policy
+        private readonly ObjectID oid = ObjectID.FromString("3Y7k2oTXzAHSzaiMHnFJDBuaX6SxuByVPdRUsYcvYUbs");//small
         // private readonly ObjectID oid = ObjectID.FromString("");//big
         // private readonly ObjectID oid = ObjectID.FromString("JDYccUD7eSk3MNhdGWRK3Hw3vgfSjnBj3KuvopE87m6P");//split
         // private readonly ContainerID cid = ContainerID.FromString("GxDJqnCTJBYGqKRqo6AbNWwmmj8jNNmeEX88cbrB1Myu");//with others in policy 
         // private readonly ObjectID oid = ObjectID.FromString("EBYdX5bZtGa1PxjwbWrR2pcsgubhn7h3NhWeP4YGWAL6");//small
         // private readonly ContainerID cid = ContainerID.FromString("9meDqcox8VAyC6qBomYAevA8rM79idsBxbYMcr5qJb4R");// only one go node
         // private readonly ObjectID oid = ObjectID.FromString("6GXG98kCZoZLmU3CBeYoNfZcQ4QQr25cLc3YzQKeZbD4"); //small
-        private readonly ECDsa key = "Kzj1LbTtmfbyJjn4cZhD6U4pdq74iHcmKmGRRBiLQoQzPBRWLEKz".LoadWif();//s1 storage NiXweMv91Vz512bQw7jFNHAGBg8upVS8Qo
+        private readonly ECDsa key = "Kzj1LbTtmfbyJjn4cZhD6U4pdq74iHcmKmGRRBiLQoQzPBRWLEKz".LoadWif();//s1 storage NiXweMv91Vz512bQw7jFNHAGBg8upVS8Qo testnet
         // private readonly ECDsa key = "Kz3KJfsLRjwEGLbhZXGjiZ2jbDgziTj5w3sUVVT8JtLGHdCn5jXJ".LoadWif();//other NbVGqFbFADgKYiiJhUbj3tYKibwtraWRmT
         // private readonly ECDsa key2 = "KxyjQ8eUa4FHt3Gvioyt1Wz29cTUrE4eTqX3yFSk1YFCsPL8uNsY".LoadWif();//ir Nhfg3TbpwogLvDGVvAvqyThbsHgoSUKwtn
         private Address Address => new(cid, oid);
